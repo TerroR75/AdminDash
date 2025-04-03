d559998929ed1cc38edb6771a262b2331ee571fc
Kam Sul <kamilsulimierski@gmail.com>
2025-04-03 17:12:49 +0200

    Added sequelize connection

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { db } from "../database.js";
 
 // Routes imports
 import employeesRoute from "./routes/employeesRouter.js";
@@ -14,6 +15,18 @@ app.use((err, req, res, next) => {
 // Routers
 app.use("/employees", employeesRoute);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+const initApp = async () => {
+  // Test the connection.
+  try {
+    await db.authenticate();
+    console.log("Connection has been established successfully.");
+
+    app.listen(port, () => {
+      console.log(`Server is running at: http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error("Unable to connect to the database:", error.original);
+  }
+};
+
+initApp();
